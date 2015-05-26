76922fd5db3ef0cce338c4131ac39d3b5e373d06
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2015-05-26 16:31:37 +0300

    Reconstructed simple http server
    Moving from CoffeeScript to JavaScript.

---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+var epilogue = require('epilogue');
+var express = require('express');
+
+var API = function API (app, sequelize) {
+  var api = express.Router();
+
+  epilogue.initialize({
+    app: api,
+    sequelize: sequelize
+  });
+
+  app.use('/api', api);
+
+  this.sequelize = sequelize;
+}
+
+module.exports = API;
---
+++ src/server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var http = require('http');
+var Sequelize = require('sequelize');
+
+var API = require('./api');
+
+var Server = function Server (callback) {
+  var callbackIsFunction = callback instanceof Function;
+  if (!callbackIsFunction) callback = function () {};
+
+  var app = express();
+  var sequelize = new Sequelize('db', null, null, {
+    dialect: 'sqlite'
+  });
+
+  var api = new API(app, sequelize);
+
+  app.use('/screen', express.static(__dirname + '/screen/public'));
+
+  var server = http.createServer(app).listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Server listening at http://%s:%s', host, port);
+
+    callback();
+  });
+
+  this.app = app;
+  this.api = api;
+  this.sequelize = sequelize;
+}
+
+module.exports = Server;
