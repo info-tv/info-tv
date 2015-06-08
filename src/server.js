dbd9976331d76f8ca3aa6d411ac446a16310b610
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2015-06-08 22:00:27 +0300

    Refactred code

--- src/server.js
+++ src/server.js
@@ -7,21 +7,25 @@ var API = require('./api');
 var models = require('./models');
 
 var Server = function Server(callback) {
-  var callbackIsFunction = callback instanceof Function;
-  if (!callbackIsFunction) callback = function () {};
+  if (typeof callback !== 'function') callback = function () {};
 
+  // init express application
   var app = express();
+  app.use(bodyParser.json());
+
+  // init sequelize
   var sequelize = new Sequelize('db', null, null, {
     dialect: 'sqlite'
   });
 
-  app.use(bodyParser.json());
-
+  // init api and models
   var api = new API(app, sequelize);
   models.init(sequelize);
 
+  // sync database
   sequelize.sync();
 
+  // load static folders
   app.use('/screen', express.static(__dirname + '/screen/public'));
 
   var server = http.createServer(app).listen(3000, function () {
