a0eecf73b58490c6cc826c3baffde2014bdd1900
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2015-08-31 21:11:18 +0300

    Tested apiDoc in screens controller

--- src/controllers/v1/screens.js
+++ src/controllers/v1/screens.js
@@ -17,10 +17,52 @@ module.exports = {
   findById: AbstractController.findById.bind(this, Screen),
 
   /**
-   * GET /api/v1/screens
+   * @api {get} /api/v1/screens List screens
+   * @apiName GetScreens
+   * @apiGroup Screen
    *
-   * 200 OK                    - if there was no problems
-   * 500 Internal Server Error - if there was any problems
+   * @apiSuccess {Object[]} screens                  List of screens
+   * @apiSuccess {Number}   screens.ObjectId         Object id of screen
+   * @apiSuccess {Number}   screens.id               id
+   * @apiSuccess {String}   [screens.name=""]        name
+   * @apiSuccess {String}   [screens.description=""] description
+   * @apiSuccess {String}   [screens.location=""]    physical or logical location
+   * @apiSuccess {Number}   [screens.width_mm=0]     width in millimetres
+   * @apiSuccess {Number}   [screens.height_mm=0]    height in millimetres
+   * @apiSuccess {Number}   [screens.width_px=0]     width in pixels
+   * @apiSuccess {Number}   [screens.height_px=0]    height in pixels
+   * @apiSuccess {Object}   [screens.layout={}]      layout and style in String/JSON
+   * @apiSuccess {String}   screens.createdAt        timestamp of creation in ISO format
+   * @apiSuccess {String}   screens.updatedAt        timestamp of latest update in ISO format
+   *
+   * @apiSuccessExample {json}
+   *     HTTP/1.1 200 OK
+   *     [
+   *       {
+   *         "ObjectId": 12,
+   *         "id": 3,
+   *         "width_mm": 200,
+   *         "height_mm": 113,
+   *         "width_px": 1920,
+   *         "height_px": 1080,
+   *         "name": "SD3",
+   *         "description": "Lorem ipsum dolor sit amet...",
+   *         "location": "Stage D",
+   *         "layout": {},
+   *         "createdAt": "2015-08-31T14:35:21.202Z",
+   *         "updatedAt": "2015-08-31T14:35:21.202Z"
+   *       }
+   *     ]
+   *
+   *
+   * @apiError (500) InternalServerError
+   *
+   * @apiErrorExample {json}
+   *     HTTP/1.1 500 Internal Server Error
+   *     {
+   *       "code": "InternalError",
+   *       "message": "Not so useful error message"
+   *     }
    */
   index: AbstractController.index.bind(this, Screen),
 
