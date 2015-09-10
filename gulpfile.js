e8cc52a96755b1d73e4b29ded45d03c391bd2014
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2015-09-10 23:04:25 +0300

    Added migrate task to gulp
    'gulp migrate' runs all pending migrations
    'gulp' and 'gulp test' have migrate in their deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var apidoc = require('gulp-apidoc');
+var Bluebird = require('bluebird');
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var Sequelize = require('sequelize');
+var Umzug = require('umzug');
 
 /**
  * Store all needed paths here.
  */
 var paths = {
   apidoc: './apidoc/',
+  config_json: __dirname + '/config/config.json',
+  migrations: __dirname + '/src/migrations',
   src: './src',
   test_js: './test/**/*.js'
 };
@@ -22,19 +27,62 @@ gulp.task('apidoc', function (cb) {
 });
 
 /**
- * Run mocha
+ * Run all pending migrations the same way as running
+ * sequelize db:migrate --migrations-path src/migrations
+ */
+gulp.task('migrate', function () {
+  var env = process.env.NODE_ENV || 'development';
+  var config = require(paths.config_json)[env];
+  config.logging = false;
+
+  var sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+
+  var migrator = new Umzug({
+    logging: console.log,
+    storage: 'sequelize',
+    storageOptions: {
+      sequelize: sequelize,
+      tableName: 'SequelizeMeta'
+    },
+    migrations: {
+      params: [sequelize.getQueryInterface(), Sequelize],
+      path: paths.migrations,
+      pattern: /\.js$/,
+      wrap: function (fun) {
+        if (fun.length === 3) {
+          return Bluebird.promisify(fun);
+        } else {
+          return fun;
+        }
+      }
+    }
+  });
+
+  return migrator.up();
+});
+
+/**
+ * Run mocha tests
  */
 gulp.task('mocha', function () {
   return gulp.src(paths.test_js).pipe(mocha());
 });
 
 /**
- * Run all tests
+ * Run migrations and tests
+ * - migrate: migrate database to match the code
+ * - mocha: run mocha tests
+ * - apidoc: try to generate api documentation
  */
-gulp.task('test', ['mocha', 'apidoc']);
+gulp.task('test', ['migrate', 'mocha', 'apidoc']);
 
 /**
  * Run all tasks
  */
-gulp.task('default', ['apidoc', 'test']);
+gulp.task('default', ['test']);
 
